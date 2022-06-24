c4e84c8ef415b45ec3b7905bd8c13ec99d5247d9
unknown <emilianoagu@hotmail.com>
2022-06-24 18:38:42 -0300

    Brand new component(ItemCounts working!)

--- my-ecommerce/src/components/ItemCount.js
+++ my-ecommerce/src/components/ItemCount.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/itemcount.css'
 
-export default function ItemCount(props){
-    return(
+export default function ItemCount({ stock }) {
+    var stock = 7
+    const [num, setNum] = useState(0)
+
+    const add = () => num < stock ? setNum(num + 1) : alert(`Stock of product:${num}`)
+    
+
+    const sub = () => num > 0 ? setNum(num - 1) : alert(`Empty cart`)
+    
+
+    const reset = () => setNum(0)
+    
+
+    return (
         <div className='counter-wrapper'>
-            <p>Nombre del gin</p>
+            <p>Gin selected</p>
             <div className='btn-container'>
-                <img id='dwn' src={require('../img/icons/chevron-dwn.png')} alt='Button down'/>
-                <p id='count'>0</p>
-                <img id='up' src={require('../img/icons/chevron-up.png')} alt='Button up'/> 
+                <button className='btn' onClick={sub}><img id='dwn' src={require('../img/icons/chevron-dwn.png')} alt='Button down' /></button>
+                <p id='count'>{num}</p>
+                <button className='btn' onClick={add}><img id='up' src={require('../img/icons/chevron-up.png')} alt='Button up' /></button>
             </div>
             <button className='add-btn'>Add to cart</button>
+            <button className='reset-btn' onClick={reset}>Reset</button>
         </div>
     )
 }
