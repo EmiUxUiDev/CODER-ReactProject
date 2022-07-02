f00fef697381abbe93eeef1fc9d4149d604fa02b
unknown <emilianoagu@hotmail.com>
2022-07-02 16:00:35 -0300

    I created a component to find an specific item or product of the whole api result, it works under the onChange event of input html tag and his attribute value.

--- my-ecommerce/src/components/ItemListContainer.js
+++ my-ecommerce/src/components/ItemListContainer.js
@@ -2,27 +2,28 @@ import { useEffect, useState } from 'react'
 import '../styles/itemlistcontainer.css'
 import Info from './Info.js';
 import ItemList from './ItemList.js'
+import SearchItem from './SearchItem.js'
 
 export default function ItemListContainer() {
 
     const [products, setproducts] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
-            useEffect(()=>{
-                setIsLoading(true)
-                setTimeout(()=>{
-                    fetch('https://rickandmortyapi.com/api/character')
-                    .then((res) => res.json())
-                    .then((data) => setproducts(data.results))
+    useEffect(() => {
+        setIsLoading(true)
+        setTimeout(() => {
+            fetch('https://rickandmortyapi.com/api/character')
+                .then((res) => res.json())
+                .then((data) => setproducts(data.results))
                 .catch(error => console.log(error))
-                setIsLoading(false)
-                }, 2000)  
-            },[])
-        
-        
+            setIsLoading(false)
+        }, 2000)
+    }, [])
+
+
 
     return (
-            <div className='wrapper-container'>
+        <div className='wrapper-container'>
             <section className='item-section'>
                 <div className='item-text'>
                     <h1>Enjoy your Gin, do it your way...</h1>
@@ -31,8 +32,9 @@ export default function ItemListContainer() {
                 </div>
                 <img className='item-img' src={require('../img/gyn-cup.png')} alt='Fresh gin cup served' />
             </section>
+            <SearchItem />
             <Info />
-            {isLoading && <img className='loader' src='tocLoader.gif' alt='Gif loader'/>}
+            {isLoading && <img className='loader' src='tocLoader.gif' alt='Gif loader' />}
             <ItemList gins={products} />
         </div>
     )
---
+++ my-ecommerce/src/components/SearchItem.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../styles/searchitem.css'
+import { useState, useEffect } from 'react'
+import { Link} from 'react-router-dom'
+
+export default function SearchItem() {
+  const [gins, setGin] = useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setGin(data.results)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+// Funcion que toma valor de entrada del input con hook useState
+  const searcher = (event) => {
+    setSearch(event.target.value)
+  }
+
+
+  let ginResult = []
+  // Filtro lo devuelto por el fetch, usando como argumento el valor del value del input
+  if (!search) {
+    ginResult = []
+  } else {
+    ginResult = gins.filter((elem) => elem.name.toLowerCase().includes(search.toLocaleLowerCase()))
+  }
+
+
+  return (
+    <section className='main-wrapper'>
+      <div className='title-wrapper'>
+        <h4>-- Gin finder, choose one that suits you best ! --</h4>
+      </div>
+      <div className='input-wrapper'> 
+        {/* Bajo el evento onChange, invoco la funcion que toma los valores q van ingresando al input y me setean el hook */}
+        <input type='text' value={search} onChange={searcher} className='search-el' placeholder='Search'></input>
+      </div>
+      <div className='result-wrapper'>
+        {ginResult.map((gin) => (
+          <ul>
+            <Link to={`/Detail/${gin.id}`}><li>{`Id: ${gin.id} - name: ${gin.name} - gender: ${gin.gender} - origin: ${gin.origin.name}`}</li></Link>
+          </ul>
+        ))}
+      </div>
+    </section>
+  )
+}
